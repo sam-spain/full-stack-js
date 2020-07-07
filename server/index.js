2ec753ace0597124618f75c220a14b1a4d81f1aa
sam <sam@samspain.com>
2020-07-07 21:34:14 +0100

    Show html

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-import Express from 'express'
+import Express, { response } from 'express'
 import Mongoose from 'mongoose'
 import config from '@config'
+import path from 'path'
 import v1Router from '@routes'
+import Webpack from 'webpack'
+import WebpackConfig from '../webpack.config'
+import WebpackDevMiddleware from 'webpack-dev-middleware'
 
 Mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const app = Express()
+const compiler = Webpack(WebpackConfig)
+app.use(WebpackDevMiddleware(compiler))
 app.use(v1Router)
-app.listen(3000, () => {
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
+app.listen(8081, () => {
     console.log(`connected to DB: ${config.databaseUrl}`)
 })
