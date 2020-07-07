287d8271b9d653918d5139b2099d5243dc0064ed
sam <sam@samspain.com>
2020-07-07 20:54:09 +0100

    Set up API versioning and setting up auth routes

---
+++ server/controllers/v1/auth.controller.js
@@ -0,0 +1,10 @@
+const { request, response } = require("express")
+
+const login = (request, response) => {}
+
+const register = (request, response) => {}
+
+export default {
+    login,
+    register
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import Express from 'express';
 import Mongoose from 'mongoose';
 import config from './config';
+import v1Router from './routes';
 
-Mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+Mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const app = Express()
-
+app.use(v1Router)
 app.listen(3000, () => {
     console.log(`connected to DB: ${config.databaseUrl}`);
 })
